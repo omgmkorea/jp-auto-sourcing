eccb74591901c3b2ab3493d5f21e3f71748e93b9
omgmkorea <omgm.korea@gmail.com>
2026-03-05 14:20:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 import os
-import base64
+import smtplib
 from email.mime.text import MIMEText
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 
 EMAIL_ACCOUNT = os.getenv("EMAIL_ACCOUNT")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
-
-flow = InstalledAppFlow.from_client_secrets_file(
-    "credentials.json", SCOPES
-)
-
-creds = flow.run_local_server(port=0)
-
-service = build("gmail", "v1", credentials=creds)
+print("JP Sourcing System Running")
 
 message = MIMEText("JP Auto Sourcing Test Email")
 
-message["to"] = EMAIL_ACCOUNT
-message["from"] = EMAIL_ACCOUNT
-message["subject"] = "JP Sourcing Test"
+message["Subject"] = "JP Sourcing Test"
+message["From"] = EMAIL_ACCOUNT
+message["To"] = EMAIL_ACCOUNT
 
-raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(EMAIL_ACCOUNT, EMAIL_PASSWORD)
 
-body = {"raw": raw}
+server.sendmail(
+    EMAIL_ACCOUNT,
+    EMAIL_ACCOUNT,
+    message.as_string()
+)
 
-service.users().messages().send(
-    userId="me",
-    body=body
-).execute()
+server.quit()
 
 print("Email sent successfully")
