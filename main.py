8b9b523132796a55db30afa258c741f0e3500ed7
omgmkorea <omgm.korea@gmail.com>
2026-03-05 14:09:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import base64
 import os
+import base64
 from email.mime.text import MIMEText
-from google.oauth2 import service_account
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 EMAIL_ACCOUNT = os.getenv("EMAIL_ACCOUNT")
 
-message_text = "JP Auto Sourcing Test Email"
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-message = MIMEText(message_text)
+flow = InstalledAppFlow.from_client_secrets_file(
+    "credentials.json", SCOPES
+)
+
+creds = flow.run_local_server(port=0)
+
+service = build("gmail", "v1", credentials=creds)
+
+message = MIMEText("JP Auto Sourcing Test Email")
 
 message["to"] = EMAIL_ACCOUNT
 message["from"] = EMAIL_ACCOUNT
@@ -16,8 +25,11 @@ message["subject"] = "JP Sourcing Test"
 
 raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-service = build("gmail", "v1")
-
 body = {"raw": raw}
 
-print("Email send simulation complete")
+service.users().messages().send(
+    userId="me",
+    body=body
+).execute()
+
+print("Email sent successfully")
